e2d8d4b0a0d0749a1b26d903865877b13c238938
CubedWolf <willberlovegame@gmail.com>
2025-05-24 00:25:55 -0400

    wawa

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import os
 from dotenv import load_dotenv
+import discord
+from discord import app_commands
 
 
 load_dotenv(dotenv_path="token.env")
 token = os.environ["token"]
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+    print(f"logged in as {client.user}")
+
+
+
+client.run(token)
