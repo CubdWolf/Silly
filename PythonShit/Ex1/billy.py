af28ea85db5fa07a650481a21705c35cdbededb6
CubedWolf <willberlovegame@gmail.com>
2025-05-24 01:09:22 -0400

    Co-authored-by: Samecho <Samecho@users.noreply.github.com>

---
+++ PythonShit/Ex1/billy.py
@@ -0,0 +1,4 @@
+name = input("what is name")
+age = input("what is age")
+in100 = 2025+(100-int(age))
+print(f"tou will be 100 in {in100}")
---
+++ PythonShit/Ex1/same.py
@@ -0,0 +1,4 @@
+age = int(input("What's your age"))
+name = age = input("What's your name")
+
+print("You will be "+ str(100 - age + 2025) + " years old when you are 100")
